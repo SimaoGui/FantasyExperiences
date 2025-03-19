2ed51ac654ec5482f189d3370b3b54b16ab34712
Guilherme da Silva Simão <guisimao2003@gmail.com>
2025-03-19 17:06:23 -0300

    pagesDeploy

--- src/app.js
+++ src/app.js
@@ -1,20 +1,16 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/home.js";
 
 function App() {
   return (
     <Router basename="/FantasyExperiences">
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
-
         {/* <Route path="/sobre" element={<AboutPage />} /> */}
-
-        <Route path="/SimaoGui/FantasyExperiences" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/home" />} />
       </Routes>
-
     </Router>
-
   );
 }
 
